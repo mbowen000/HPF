a099d4ee40026cb8d6cbbd9034186914e3cf3165
jrubins <jonrubins@gmail.com>
2014-12-27 14:50:32 -0600

    Bootstrap
    Included Bootstrap and jQuery.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -26,6 +26,9 @@ var jsFilesToInject = [
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
+  // Load jQuery next since we have dependencies on jQuery
+  'js/dependencies/jquery-2.1.3.min.js',
+
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
